4b70812ed68c5d6d8f27dbd7e832aea27fa72405
10Develops <32372072+10Develops@users.noreply.github.com>
2019-12-06 03:38:32 +0400

    New version 0.3!

---
+++ CS Tabbed Web Browser/Classes/WebBrowserTabControl.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Drawing;
+namespace CS_Tabbed_Web_Browser
+{
+    /// <summary>
+    /// A <b>TabControl</b> designed to display <see cref="WebBrowserTabPage" /> objects.
+    /// </summary>
+    public class WebBrowserTabControl : TabControl
+    {
+        [DllImport("user32.dll")]
+        private static extern IntPtr SendMessage(IntPtr hWnd, int msg, IntPtr wp, IntPtr lp);
+        private const int TCM_SETMINTABWIDTH = 0x1300 + 49;
+        #region Properties
+
+        public WebBrowserTabControl()
+        {
+            DrawMode = TabDrawMode.Normal;
+        }
+
+        /// <summary>
+        /// Gets the currently selected tab page.
+        /// </summary>
+        /// <value>
+        /// The <see cref="WebBrowserTabPage" /> that is currently selected.
+        /// </value>
+        public WebBrowserTabPage SelectedWebBrowserTagPage
+        {
+            get
+            {
+                return (WebBrowserTabPage)this.SelectedTab;
+            }
+        }
+
+        /// <summary>
+        /// Gets the currently selected web browser.
+        /// </summary>
+        /// <value>
+        /// The <b>WebBrowser</b> control on the currently selected tab page.
+        /// </value>
+        public WebBrowserCore SelectedWebBrowser
+        {
+            get
+            {
+                return this.SelectedWebBrowserTagPage.Browser;
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Adds a new, empty page to the tab control.
+        /// </summary>
+        /// <returns> 
+        /// The <see cref="WebBrowserTabPage" /> that was added.
+        /// </returns>
+        public WebBrowserTabPage AddTab()
+        {
+            WebBrowserTabPage tab = new WebBrowserTabPage();
+            if (TabCount == 0)
+            {
+                TabPages.Insert(TabCount, tab);
+            }
+            else
+            {
+                TabPages.Insert(TabCount - 1, tab);
+            }
+            this.SelectedTab = tab;
+            return tab;
+        }
+
+        /// <summary>
+        /// Adds a new page to the tab control and navigates to the specified address.
+        /// </summary>
+        /// <param name="address">
+        /// The address of the page to display in the new tab.
+        /// </param>
+        /// <returns>
+        /// The <see cref="WebBrowserTabPage" /> that was added.
+        /// </returns>
+        public WebBrowserTabPage AddTab(string address)
+        {
+            WebBrowserTabPage tab = this.AddTab();
+            tab.Browser.Navigate(address);
+            return tab;
+        }
+
+        /// <summary>
+        /// Removes the currently selected tab from the tab control.
+        /// </summary>
+        /// <returns>
+        /// <b>True</b> if the current tab was removed; otherwise, <b>False</b>.
+        /// </returns>
+        /// <remarks>
+        /// There must always be at least one tab so the last tab cannot be removed.
+        /// </remarks>
+        public bool RemoveCurrentTab()
+        {
+            bool canRemove = this.TabCount > 2;
+
+            if (canRemove)
+            {
+                WebBrowserTabPage tab = this.SelectedWebBrowserTagPage;
+                this.TabPages.Remove(tab);
+                tab.Dispose();
+            }
+
+            return canRemove;
+        }
+
+        #endregion
+
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            SendMessage(Handle, TCM_SETMINTABWIDTH, IntPtr.Zero, (IntPtr)30);
+            base.OnHandleCreated(e);
+        }
+
+        protected override void OnDrawItem(DrawItemEventArgs e)
+        {
+            base.OnDrawItem(e);
+
+            //This code will render a "x" mark at the end of the Tab caption. 
+            e.Graphics.DrawString("x", e.Font, Brushes.Black, e.Bounds.Right - 10, e.Bounds.Top + 6);
+            e.Graphics.DrawString(this.TabPages[e.Index].Text, e.Font, Brushes.Black, e.Bounds.Left, e.Bounds.Top + 6);
+            e.DrawFocusRectangle();
+
+        }
+    }
+}
---
+++ CS Tabbed Web Browser/Windows/OptionsDialogue.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using CS_Tabbed_Web_Browser.Properties;
+namespace CS_Tabbed_Web_Browser
+{
+    public partial class OptionsDialogue : Form
+    {
+        #region Variables
+
+        /// <summary>
+        /// The addressed currently displayed in the browser.
+        /// </summary>
+        private string currentAddress;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="OptionsDialogue" /> class.
+        /// </summary>
+        /// <param name="currentAddress">
+        /// A <b>String</b> containing the address of the page currently displayed in the browser.
+        /// </param>
+        public OptionsDialogue(string currentAddress)
+        {
+            // This call is required by the Windows Form Designer.
+            InitializeComponent();
+
+            // Add any initialization after the InitializeComponent() call.
+
+            this.currentAddress = currentAddress;
+        }
+
+        #endregion
+
+        #region Event Handlers
+        private void OptionsDialogue_Load(object sender, EventArgs e)
+        {
+            // Load the current options
+            this.homePageText.Text = Settings.Default.HomePage;
+            this.startupWithHomePageToggle.Checked = Settings.Default.StartWithHomePage;
+            showHomeButtonToggle.Checked = Settings.Default.ShowHomePage;
+            dynamicStatusBarToggle.Checked = Settings.Default.ShowStatusBar;
+            dynamicProgressBarToggle.Checked = Settings.Default.ShowProgressBar;
+            cbSearchEngine.SelectedIndex = Settings.Default.DefaultSearchEngine;
+
+            if(Settings.Default.UseProxy == true)
+            {
+                rbUseProxy.Checked = true;
+                tbProxy.Text = Settings.Default.Proxy;
+            }
+            else
+            {
+                rbNoUseProxy.Checked = true;
+                tbProxy.Enabled = false;
+            }
+        }
+
+        private void useCurrentAsHomePageButton_Click(object sender, EventArgs e)
+        {
+            this.homePageText.Text = this.currentAddress;
+        }
+
+        private void useBlankAsHomePageButton_Click(object sender, EventArgs e)
+        {
+            this.homePageText.Clear();
+        }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            // Save the new settings
+            Settings.Default.HomePage = this.homePageText.Text;
+            Settings.Default.StartWithHomePage = this.startupWithHomePageToggle.Checked;
+            Settings.Default.ShowHomePage = showHomeButtonToggle.Checked;
+            Settings.Default.EnableDynamicStatusBar = dynamicStatusBarToggle.Checked;
+            Settings.Default.EnableDynamicProgressBar = dynamicProgressBarToggle.Checked;
+            Settings.Default.DefaultSearchEngine = cbSearchEngine.SelectedIndex;
+
+            if (rbUseProxy.Checked == true)
+            {
+                if (!string.IsNullOrEmpty(tbProxy.Text))
+                {
+                    var ValidIpAddressPortRegex = @"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]):[\d]+$";
+                    if (System.Text.RegularExpressions.Regex.IsMatch(tbProxy.Text, ValidIpAddressPortRegex))
+                    {
+                        Settings.Default.Proxy = tbProxy.Text;
+                        Settings.Default.UseProxy = true;
+                    }
+                    else
+                    {
+                        MessageBox.Show("IP:PORT url not valid!","Information");
+                    }
+                }
+                else
+                {
+                    Settings.Default.UseProxy = false;
+                }
+            }
+            else
+            {
+                if (!string.IsNullOrEmpty(tbProxy.Text))
+                {
+                    Settings.Default.Proxy = tbProxy.Text;
+                }
+                    Settings.Default.UseProxy = false;
+            }
+            Settings.Default.Save();
+            Close();
+        }
+        private void rbNoUseProxy_CheckedChanged(object sender, EventArgs e)
+        {
+            tbProxy.Enabled = !rbNoUseProxy.Checked;
+        }
+
+        #endregion
+    }
+}
